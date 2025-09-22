44f6930de9a98d23fc5226cf9203481866c4c5e7
mrvsh879 <vladislavshatrov61@gmail.com>
2025-09-22 11:06:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ⚠️ base должен совпадать с именем репозитория на GitHub
+// У тебя репозиторий называется "sales-script"
 export default defineConfig({
+  base: '/sales-script/',
   plugins: [react()],
 })
